e158e91be1ca1a78dfc34cce2f67d65ac914f09a
Salla <salla.tero@gmail.com>
2019-03-24 19:38:43 +0200

    backend step 8 valmis

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
 app.use(cors())
+morgan.token('DATA', function (req, res) {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    }else{
+        return null
+    }
+})
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.DATA(req, res)
+    ].join(' ')
+}))
 
 let persons = [
     {
