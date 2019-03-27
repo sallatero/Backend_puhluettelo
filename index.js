8126c7685c78f891e504479cdc6c4bf5491b8d14
Salla <salla.tero@gmail.com>
2019-03-27 11:15:24 +0200

    virheenkäsittely keskitetty middlewareen

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const Person = require('./models/person')
 
 //Puhelinluettelo Backend
 
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(cors())
-app.use(express.static('build'))
+
 morgan.token('DATA', function (req, res) {
     if (req.method === 'POST') {
         return JSON.stringify(req.body)
@@ -43,31 +44,29 @@ app.get('/api/persons', (req, res) => {
     //res.json(persons)
     Person.find({})
     .then(all => {
-        console.log('res.json rivi 46');
+        console.log('ihmiset löytyivät')
         res.json(all.map(p => p.toJSON()))
-        return
     })
     .catch(error => {
         console.log(error)
-        console.log('res.status(404).end rivi 52');
-        res.status(404).end()
+        next(error)
     })
 })
 
 //Palauttaa id:n mukaisen resurssin, jos se löytyy, muuten 404 not found
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    
-    Person.find({id: id})
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
     .then(pers => {
-        console.log('res.json rivi 63');
-        res.json(pers.toJSON())
-        return
+        if (pers) {
+            console.log(`Tyyppi id:llä ${req.params.id} löytyi`);
+            res.json(pers.toJSON())
+        } else {
+            res.status(204).end()
+        }
     })
     .catch(error => {
         console.log(error)
-        console.log('res.status(404).end rivi 69');
-        res.status(404).end()
+        next(error)
     })
 })
 
@@ -75,8 +74,12 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
     Person.findByIdAndRemove(req.params.id)
         .then(person => {
-            console.log('tietokannasta löytyi id:llä ', req.params.id);
-            res.status(204).end()
+            if (person) {
+                console.log('tietokannasta löytyi id:llä ', req.params.id);
+                res.status(204).end()
+            } else {
+                res.status(204).end()
+            }
         })
        .catch(error => {
             console.log('Error in delete')
@@ -89,8 +92,7 @@ app.post('/api/persons', (req, res) => {
     const body = req.body
 
     if (!body.name || !body.number) {
-        console.log('res.status(400).json rivi 97');
-        return res.status(400).json({ //res.json->FINISHED
+        res.status(400).json({ //res.json->FINISHED
             error: 'content missing'
         })
     }
@@ -118,16 +120,41 @@ app.post('/api/persons', (req, res) => {
     newPerson.save()
     .then(savedP => {
         const jsonP = savedP.toJSON()
-        console.log('res.status(200).json rivi 124');
         res.status(200).json(jsonP)
-        return
     })
     .catch(error => {
         console.log(error)
-        console.log('res.status(404) rivi 133');
-        return res.status(404)
+        next(error)
     })
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const pers = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(req.params.id, pers, {new:true})
+    .then(updatedPers => {
+        res.json(updatedPers.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+
+//Unknown endpoint
+//ota käyttöön
+
+//Error handler
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message)
+    if (error.name === 'CastError' && error.kind == 'ObjectId') {
+        return res.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
